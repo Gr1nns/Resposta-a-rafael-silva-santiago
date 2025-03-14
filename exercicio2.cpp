1e86f548927909b3705824373cf17db9cf10bf95
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-14 15:18:34 -0300

    Create exercicio2.cpp

---
+++ exercicio2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale>
+#include <string>
+using namespace std;
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    string n1;
+ cout << "Digite seu nome: ";
+ cin >> n1;
+ cout << "Olá "<< n1 << ", é um prazer te conhecer!" <<endl ; 
+
+return 0;
+}
