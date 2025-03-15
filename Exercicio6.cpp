f8b1a0ddf9754648d8cb07161acd00e950968966
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-15 07:15:40 -0300

    Update Exercicio6.cpp

--- Exercicio6.cpp
+++ Exercicio6.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+#include <locale>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+int n, a, s;
+cout << "Digite um número inteiro: ";
+	cin >> n;
+	a = n - 1;
+	s = n + 1;
+cout << "O antecessor de " << n << " é "<< a << " e o sucessor sera " << s << endl;
+	return 0;
+}
