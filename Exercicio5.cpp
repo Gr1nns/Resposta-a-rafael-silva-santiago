bdfa1d4611327c40476bc32ccdc2240212007c01
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-14 21:38:55 -0300

    Update Exercicio5.cpp

--- Exercicio5.cpp
+++ Exercicio5.cpp
@@ -1,1 +1,13 @@
-
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	float m, ms, msn;
+	cout << "Digite a nota 1: ";
+		cin >> m;
+	cout << "Digite a nota 2: ";
+	cin >> ms;
+	msn = (m + ms) / 2;
+		cout << "A média entre "<< fixed << setprecision(2) << m << " e " << fixed << setprecision(2)<< ms << " é igual a : " << msn << endl;
+	return 0;
+}
