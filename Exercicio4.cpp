2fbce4e36d7524dc3036e4cce9e7e4803f7e013d
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-14 21:19:54 -0300

    Update Exercicio4.cpp

--- Exercicio4.cpp
+++ Exercicio4.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <locale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	int n, n1, n2;
+	cout << "Digite um número inteiro: ";
+	cin >> n;
+	cout << "Digite outro: ";
+	cin >> n1;
+	n2 = n + n1;
+	cout << "A soma de " << n << " + " << n1 << " é igual a: " << n2 << endl;
+	return 0;
+}
