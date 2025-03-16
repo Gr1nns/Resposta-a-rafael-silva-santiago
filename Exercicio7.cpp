e7c488e62fb6abd4a5f9b406e4407cb615bfedfa
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-16 15:36:11 -0300

    Update Exercicio7.cpp

--- Exercicio7.cpp
+++ Exercicio7.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <locale>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+float n, d, tp;
+cout << "Digite um número inteiro: ";
+	cin >> n;
+	d = n * 2;
+	tp = n / 3;
+cout << "O dobro de "<< n <<" é "<< d <<endl;
+cout << "A terça parte de " << n << " é " << fixed << setprecision(2) << tp << endl;
+	return 0;
+}
