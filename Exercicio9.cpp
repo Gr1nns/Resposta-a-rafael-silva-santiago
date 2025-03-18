11c404b2325d70e652dd2376fdd40815f0fbccb4
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-18 17:42:22 -0300

    Update Exercicio9.cpp

--- Exercicio9.cpp
+++ Exercicio9.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <locale>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+long double m, m1;
+cout << "Digite a quantidade em Reais que vc tem na carteira: R$";
+cin >> m;
+m1 = m / 3.45;
+cout << "A quantidade de Dolares que você é capaz de comprar é: $" << fixed << setprecision(2) << m1 << endl;
+return 0;
+}
