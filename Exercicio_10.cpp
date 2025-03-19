1a34ef46d57ed01804fd49c43123dc1562e36eb3
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-19 18:44:53 -0300

    Update Exercicio_10.cpp

--- Exercicio_10.cpp
+++ Exercicio_10.cpp
@@ -1,1 +1,17 @@
-
+#include <locale>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+long double a, la, at, lt;
+cout << "Digite a Altura da parede: ";
+    cin >> a;
+cout << "Digite a Largura da parede: ";
+    cin >> la;
+    at = a * la;
+    lt = at / 2;
+cout << "A area a ser pintade é de: " << fixed << setprecision(2) << at << "M"  << endl;
+cout << "A quantidade de tinta a ser utilizada é de: Lt" << fixed << setprecision(2) <<lt << endl;
+return 0;
+}
