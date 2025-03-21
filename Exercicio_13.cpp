82d0e642d05caa710d35c4e65bc4c8be68380b16
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-21 15:00:21 -0300

    Update Exercicio_13.cpp

--- Exercicio_13.cpp
+++ Exercicio_13.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <locale>
+using namespace std;
+int main(){
+long double p, f;
+setlocale(LC_ALL,"Portuguese");
+cout << "Digite o sálario do fúncionario: ";
+  cin >> p;
+f = p+(p*0.15);
+cout << "O novo sálario do fúncionario é igual a: R$" <<fixed<<setprecision(2)<<f<< endl;
+  return 0;
+}
