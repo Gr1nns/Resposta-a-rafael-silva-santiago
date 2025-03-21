c925d9b4891715c09dd0dabcede3f4dc7637b851
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-21 14:48:08 -0300

    Update Exercicio_11.cpp

--- Exercicio_11.cpp
+++ Exercicio_11.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <locale>
+#include <iomanip>
+using namespace std;
+int main(){
+	int a, b, c, D;
+cout << "Digite o valor de A: ";
+  cin >> a;
+cout << "Digite o valor de B: ";
+  cin >> b;
+cout << "Digite o valor de C: ";
+  cin >> c;
+D = (b*b)-4*a*c;
+cout << "Analisando os valores recebidos, o valor de delta seria: " << D << endl;
+  return 0;
+}
