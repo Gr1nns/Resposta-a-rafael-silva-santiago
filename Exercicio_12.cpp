ce1492c20c1a923814095dd7ebd84e9ef2a07106
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-21 14:57:11 -0300

    Update Exercicio_12.cpp

--- Exercicio_12.cpp
+++ Exercicio_12.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <locale>
+using namespace std;
+int main(){
+long double p, f;
+setlocale(LC_ALL,"Portuguese");
+cout << "Digite o valor do produto: ";
+  cin >> p;
+f = p-(p*0.05);
+cout << "O novo preço do produto é igual a: R$" <<fixed<<setprecision(2)<<f<< endl;
+  return 0;
+}
