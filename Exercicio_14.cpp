6fba4404704f819b7cf41376e61344a9c2fd2aa0
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-21 15:18:16 -0300

    Update Exercicio_14.cpp

--- Exercicio_14.cpp
+++ Exercicio_14.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <locale>
+using namespace std;
+int main(){
+long double d, rd, m, sa, t;
+setlocale(LC_ALL,"Portuguese");
+cout << "Digite o total de dias que você alugou o carro: ";
+  cin >> d;
+cout << "Digite o total de km rodados: ";
+  cin >> rd;
+sa = d * 90;
+m = rd * 0.20;
+t = m + sa;
+cout << "O total que você devera pagar é de: R$"<<fixed<<setprecision(2)<<t<<endl;
+  return 0;
+}
