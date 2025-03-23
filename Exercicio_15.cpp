5e695cbf298d7c77aaec573af916d81f51426887
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-23 13:32:08 -0300

    Update Exercicio_15.cpp

--- Exercicio_15.cpp
+++ Exercicio_15.cpp
@@ -1,1 +1,13 @@
-
+#include <locale>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+long double D, SF;
+cout << "Digite a quantidade de dias trabalhados: ";
+cin >> D;
+SF = D*(25 * 8);
+cout << "O seu salario final e de aproximadente: R$" << fixed << setprecision(2) << SF << endl;
+return 0;
+}
