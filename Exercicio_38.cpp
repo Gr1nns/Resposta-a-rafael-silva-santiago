575b2b4a446a24a9961b403e988fadc9636e9c30
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-04 14:40:37 -0300

    Update Exercicio_38.cpp

--- Exercicio_38.cpp
+++ Exercicio_38.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    int a;
+    cout << "Digite 6: ";
+    cin >> a;
+    while (a < 11) {
+        cout << a << endl;
+        a++;
+        if(a == 11){
+        	cout << "11 Acabou!"<<endl;
+		}     
+    }
+    system("pause");
+    return 0;
+}
