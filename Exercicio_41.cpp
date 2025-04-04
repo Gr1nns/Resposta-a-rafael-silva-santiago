3e4987c32b5c75c58e89b714f9c6573860e42033
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-04 15:11:52 -0300

    Update Exercicio_41.cpp

--- Exercicio_41.cpp
+++ Exercicio_41.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    int a;
+    cout << "Digite 100: ";
+    cin >> a;
+    while (a <= 100) {
+        cout << a << endl;
+        a-=5;
+        if(a == 0){
+        	cout << "0 Acabou!"<<endl;
+        	break;
+		}
+    }
+    system("pause");
+    return 0;
+}
