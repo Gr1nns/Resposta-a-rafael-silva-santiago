441df255b16a9fdbc41ee194bdd1cf9152362093
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-04 15:01:11 -0300

    Update Exercicio_40.cpp

--- Exercicio_40.cpp
+++ Exercicio_40.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    int a;
+    cout << "Digite 0: ";
+    cin >> a;
+    while (a < 18) {
+        cout << a << endl;
+        a+=3;
+        if(a == 18){
+        	cout << "18 Acabou!"<<endl;
+		}
+    }
+    system("pause");
+    return 0;
+}
