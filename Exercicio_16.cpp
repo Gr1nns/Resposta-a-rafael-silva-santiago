eaf6eed7c8cce165b2f1bf8d34476016c00783ee
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-11 13:47:28 -0300

    Update Exercicio_16.cpp

--- Exercicio_16.cpp
+++ Exercicio_16.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main(){
+	long double a, b, c;
+	cout<<"Quantos cigarros vc fuma por dia?: ";
+	cin>>a;
+	cout<<"Vc ja fumou por quantos anos?: ";
+	cin>>b;
+	c = a * 365 * b * (10.0 / 1440.0);
+	cout<<"Vc perdeu: "<<fixed<<setprecision(0)<<c<<" dias de vida."<<endl;
+}
