029199d24a5d1ca5807da03954722c727ecb9647
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-11 14:05:23 -0300

    Update Exercicio_39.cpp

--- Exercicio_39.cpp
+++ Exercicio_39.cpp
@@ -2,18 +2,15 @@
 #include <iomanip>
 #include <string>
 using namespace std;
-int main() {
+int main(){
     int a;
+	
     cout << "Digite 10: ";
     cin >> a;
-    while (a <= 10) {
+    while(a > 3){
         cout << a << endl;
-        a-=1;
-        if(a == 3){
-        	cout << "3 Acabou!"<<endl;
-        	break;
-		}
+        a--;
     }
-    system("pause");
+    cout << a << " Acabou!" << endl;
     return 0;
 }
