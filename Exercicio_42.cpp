fd93abc67bb7dcb7515e218c0b198d9c88e493c0
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-11 14:14:08 -0300

    Update Exercicio_42.cpp

--- Exercicio_42.cpp
+++ Exercicio_42.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
+int main() {
+    int a, b;
+    cout << "Digite um numero inteiro e positivo: ";
+    cin >> a;
+    cout << "Digite ate que numero positivo e inteiro, que sera a quantidade de repeticoes: ";
+    cin >> b;
+    
+    while (a < b) {
+        cout << a << endl;
+        a++;
+    }
+    
+    cout << a << " Acabou!" << endl;
+    
+    system("pause");
+    return 0;
+}
