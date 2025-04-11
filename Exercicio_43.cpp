f1cedf0e95297098ebf6b04e59ba8db2333fc282
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-11 14:30:20 -0300

    Update Exercicio_43.cpp

--- Exercicio_43.cpp
+++ Exercicio_43.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
+int main() {
+    int a, b;
+    cout << "Digite 30: ";
+    cin >> a;
+    b = a / 4; 
+
+    while (a >= 1) {
+    
+        if (a % 4 == 0)
+            cout << "[" << a << "]" << endl;
+        else
+            cout << a << endl;
+        a--;
+    }
+    system("pause");
+    return 0;
+}
