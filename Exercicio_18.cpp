3c701973d3573710115b59d67096f7dfcd658ef1
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-13 12:55:19 -0300

    Update Exercicio_18.cpp

--- Exercicio_18.cpp
+++ Exercicio_18.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "Digite o ano que vc nasceu: ";
+	cin >> a;
+	b = 2025 - a;
+	cout << "Sua idade e de " << b <<" anos" << endl;
+	if (b >= 18) {
+		cout << "Vc tem idade para votar!";
+	}
+	else{
+		cout << "Vc nao tem idade para votar ainda!";
+}
+	return 0;
+}
