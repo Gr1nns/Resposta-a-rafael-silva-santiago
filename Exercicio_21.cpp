207e0418a5334349f9d414d3b149ebb5d4fb092e
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-13 13:19:47 -0300

    Update Exercicio_21.cpp

--- Exercicio_21.cpp
+++ Exercicio_21.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    int ano;
+    cout << "Digite um ano: ";
+    cin >> ano;
+    if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+        cout << ano << " é bissexto." << endl;
+    }
+    else {
+        cout << ano << " não é bissexto." << endl;
+    }
+    return 0;
+}
