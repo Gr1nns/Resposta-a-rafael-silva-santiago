ce794e4823bf056de6b4f6475838c998ae73dd96
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-13 13:05:46 -0300

    Update Exercicio_19.cpp

--- Exercicio_19.cpp
+++ Exercicio_19.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	string n;
+	int a, b, c;
+	cout << "Digite seu nome: ";
+	cin >> n;
+	cout << "digite a nota 1: ";
+	cin >> a;
+	cout << "digite a nota 2: ";
+	cin >> b;
+	c = (a + b) / 2;
+	if (c >= 7){
+		cout << "O aluno esta acima da media, tendo como nota: " << c << endl;
+}
+	if (c < 7) {
+		cout << "O aluno esta abaixo da media, tendo como nota: " << c << endl;
+	}
+	return 0;
+}
