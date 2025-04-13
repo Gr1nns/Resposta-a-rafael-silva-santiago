41f51d14ae22cd67a5c1ea567482bc53c92be7cd
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-13 13:15:01 -0300

    Update Exercicio_20.cpp

--- Exercicio_20.cpp
+++ Exercicio_20.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	int a, b, c;
+	cout << "Digite um numero natural: ";
+	cin >> a;
+	b = a % 2;
+	if (b == 0) {
+		cout << "E par!" << endl;
+	}
+	else{
+		cout << "E impar!" << endl;
+}
+	return 0;
+}
