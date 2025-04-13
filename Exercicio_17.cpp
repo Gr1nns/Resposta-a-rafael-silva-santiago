5a5f776687315a803d40bd7a925ef890c876f452
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-13 12:49:08 -0300

    Update Exercicio_17.cpp

--- Exercicio_17.cpp
+++ Exercicio_17.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "Digite a velocidade co carro: ";
+	cin >> a;
+	b = 5 * (a - 80);
+	if (a <= 80){
+		cout << "Velocidade dentro do limite!";
+}
+  else if (a > 80) {
+	  cout << "Vc tera de pagar uma multa de R$" << b << " por ultrapassar a velocidade permitida!" << endl;
+}
+	return 0;
+}
