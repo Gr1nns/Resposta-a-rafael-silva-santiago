3ebb82ab2331bbf4bef1afcb72e7efe78c3be05b
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-24 11:49:28 -0300

    Update Exercicio_23.cpp

--- Exercicio_23.cpp
+++ Exercicio_23.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main(){
+float p, pmu, pho;
+string ge, ba;
+    cout<<"Qual seu nome ?: ";
+    cin>>ba;
+    cout  << "Você é homem ou mulher?: ";
+    cin  >> ge;
+    cout <<"Digite o valor da sua compra: ";
+    cin >> p;
+    pmu = p-(p*0.13);
+    pho = p-(p*0.05); 
+    if(ge== "homem"){ 
+           cout<<"O valor da sua compra sera de: R$"<<fixed<<setprecision(2)<<pho<<endl;
+}
+if(ge == "mulher"){
+    cout<<"O valor da sua compra sera de: R$"<<fixed<<setprecision(2)<<pmu<<endl;
+    }
+    return 0;
+    }
