54195625c2edf66a90a6b2edf0ca7dd5c451320c
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-25 15:10:47 -0300

    Update Exercicio_47.cpp

--- Exercicio_47.cpp
+++ Exercicio_47.cpp
@@ -1,1 +1,17 @@
+#include <iomanip>
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+	int a,soma;
+	cout<<"Digite 500: ";
+	cin>>a;
 
+	while(a >= 0){
+		cout<< a <<endl;	
+		soma+=a;
+		a-=50;
+}
+cout<<"A soma entre estes numeros e de: "<<soma<<endl;
+	return 0;
+}
