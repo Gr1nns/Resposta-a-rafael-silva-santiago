f2ff2a82d47e2b15d6fcb2f703c0f7046319a9c4
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-25 14:32:04 -0300

    Update Exercicio_44.cpp

--- Exercicio_44.cpp
+++ Exercicio_44.cpp
@@ -1,1 +1,21 @@
-
+#include <iomanip>
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b, c;
+	cout<<"Digite o primeiro valor: ";
+	cin>>a;
+	
+	cout<<"Digite o segundo valor: ";
+	cin>>b;
+	
+	cout<<"Digite o incremento: ";
+	cin>>c;
+	cout<<"Contagem: ";
+	while(a <= b){
+		cout<< a <<endl;
+		a+=c;
+	}
+	cout<<"Acabou!"<<endl;
+}
