1b0ab635173b0a674334a99e88c4c2d8a226cc54
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-25 15:08:50 -0300

    Update Exercicio_46.cpp

--- Exercicio_46.cpp
+++ Exercicio_46.cpp
@@ -1,1 +1,17 @@
+#include <iomanip>
+#include <string>
+#include <iostream>
+using namespace std;
+int main(){
+	int a,soma;
+	cout<<"Digite 6: ";
+	cin>>a;
 
+	while(a <= 100){
+		cout<< a <<endl;	
+		soma+=a;
+		a+=2;
+}
+cout<<"A soma entre estes numeros e de: "<<soma<<endl;
+	return 0;
+}
