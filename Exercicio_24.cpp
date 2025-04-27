3093207f91dfb28c56e7cef55e6e00bd00639cd8
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-27 17:21:28 -0300

    Update Exercicio_24.cpp

--- Exercicio_24.cpp
+++ Exercicio_24.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    long double a, b, c;
+    cout << "Digite a distancia em KM: ";
+        cin >> a;
 
+        b = a + (a * 0.5);
+        c = a + (a * 0.45);
+
+        if (a <= 200) {
+            cout << "O total ficara: R$" << fixed << setprecision(2) << b << endl;
+        }
+        else if (a > 200) {
+            cout << "O total ficara: R$" << fixed << setprecision(2) << c << endl;
+        }
+    return 0;
+}
