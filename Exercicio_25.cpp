08bb8c1ef5c735d3f67da4fdfbf7cd301551da9e
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-27 19:08:00 -0300

    Update Exercicio_25.cpp

--- Exercicio_25.cpp
+++ Exercicio_25.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    int a, b, c, dr, dt, ds;
+    cout << "Digite um dos segmentos: ";
+    cin >> a;
+    cout << "Digite outro segmento: ";
+    cin >> b;
+    cout << "Digite o ultimo segmento: ";
+    cin >> c;
 
+    dr = a + b;
+    dt = b + c;
+    ds = c + a;
+
+    if (dr > c && dt > a && ds > b) {
+        cout << "Os segmentos sao capazes de criar um triangulo!" << endl;
+    }
+    else{
+        cout << "Os segmentos nao sao capazes de criar um triangulo!" << endl;
+     }
+    return 0;
+}
