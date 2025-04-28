a8de0d7b5330028f0d8e8d72948218a541d540fd
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-27 21:33:33 -0300

    Update Exercicio_28.cpp

--- Exercicio_28.cpp
+++ Exercicio_28.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	float la, com, ar;
+    
+	cout << "Digite a largura do terreno: ";
+	cin >> la;
+	cout << "Digite o comprimento do terreno: ";
+	cin >> com;
 
+	ar = la * com;
+
+	if (ar < 100) {
+		cout << "TERRENO POPULAR!" << endl;
+	}
+	else if (ar >= 100 && ar <= 499) {
+		cout << "TERRENO MASTER!" << endl;
+	}
+	else if (ar >= 500) {
+		cout << "TERRENO VIP!" << endl;
+	}
+return 0;
+}
