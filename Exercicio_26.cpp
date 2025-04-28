ef33d055b1018dd6ee9599af64c13536b85c0ca2
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-27 21:18:23 -0300

    Update Exercicio_26.cpp

--- Exercicio_26.cpp
+++ Exercicio_26.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	int n1, n2;
+	cout << "Digite o primeiro valor: ";
+	cin >> n1;
+	cout << "Digite outro: ";
+	cin >> n2;
 
+	if (n1 > n2){
+		cout << "O primeiro valor e maior!" << endl;
+}
+	else if (n2 > n1) {
+		cout << "O segundo valor e o maior!" << endl;
+	}
+	else if (n1 == n2) {
+		cout << "Nao a valor maior, os dois sao iguais" << endl;
+}
+return 0;
+}
