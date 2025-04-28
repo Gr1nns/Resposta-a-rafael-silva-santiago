3e56931fec30c04fdafb876103096fd0664df591
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-27 21:26:23 -0300

    Update Exercicio_27.cpp

--- Exercicio_27.cpp
+++ Exercicio_27.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+	float n1, n2, me;
+	cout << "Digite a primeira nota: ";
+	cin >> n1;
+	cout << "Digite a segunda nota: ";
+	cin >> n2;
 
+	me = (n1 + n2) / 2;
+
+	if (me <= 4.9) {
+		cout << "REPROVADO!" << endl;
+	}
+	else if (me >= 5 && me <= 6.9) {
+		cout << "RECUPERACAO!" << endl;
+	}
+	else if (me >= 7) {
+		cout << "APROVADO!" << endl;
+	}
+return 0;
+}
