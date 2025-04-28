9106ba74dde6d6733b58608d0ba5b7b09db3396c
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-04-28 19:08:15 -0300

    Update Exercicio_31.cpp

--- Exercicio_31.cpp
+++ Exercicio_31.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+    string n1, n2;
+    while (true) {
+        cout << "Digite a escolha do Player 1: pedra, papel ou tesoura: ";
+        cin >> n1;
+        cout << "Digite a escolha do Player 2: pedra, papel ou tesoura: ";
+        cin >> n2;
 
+        if ((n1 == "pedra" && n2 == "tesoura") || (n1 == "tesoura" && n2 == "papel") || (n1 == "papel" && n2 == "pedra")) {
+            cout << "Player 1 ganhou!!" << endl;
+        }
+        else if ((n2 == "pedra" && n1 == "tesoura") || (n2 == "tesoura" && n1 == "papel") || (n2 == "papel" && n1 == "pedra")) {
+            cout << "Player 2 ganhou!!" << endl;
+        }
+        else if (n1 == n2) {
+            cout << "Empate!" << endl;
+        }
+        else {
+            cout << "Jogada invalida! Tente novamente." << endl;
+    }
+    }
+    return 0;
+}
