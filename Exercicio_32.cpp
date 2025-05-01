d03882d82f86e310a544ff78f28e8d31f3187588
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-01 14:09:04 -0300

    Update Exercicio_32.cpp

--- Exercicio_32.cpp
+++ Exercicio_32.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    srand(time(0));
+    int ran = (rand() % 5) + 1;
+    int ch;
 
+    cout << "Digite seu chute, um numero entre 1 e 5: " << endl;
+    cin >> ch;
+
+    if (ch == ran) {
+        cout << "Parabens, vc acertou o numero sorteado!" << endl;
+    }
+    else {
+        cout << "Voce errou o numero sorteado foi: "<<ran<< ". reinicie o programa, e tente novamente!" << endl;
+    }
+    return 0;
+}
