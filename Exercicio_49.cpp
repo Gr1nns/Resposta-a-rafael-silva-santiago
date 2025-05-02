72cb36218a9e3264fffb2bdfb428ae9fa22b661a
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-02 15:09:58 -0300

    Update Exercicio_49.cpp

--- Exercicio_49.cpp
+++ Exercicio_49.cpp
@@ -1,1 +1,19 @@
+#include <string>
+#include <iomanip>
+#include <iostream>
+using namespace std;
 
+int main() {
+    int con = 1, num;
+
+while (con <= 6) {
+    cout << "Digite o " << con << " numero: ";
+        cin >> num;
+
+if (num % 2 == 0) {
+    cout << num << " é par." << endl;
+}
+con++;
+}
+return 0;
+}
