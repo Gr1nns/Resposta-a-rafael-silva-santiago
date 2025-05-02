f8053b40eb66f46544af90299f6e5563bec4b1d4
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-02 14:20:27 -0300

    Update Exercicio_48.cpp

--- Exercicio_48.cpp
+++ Exercicio_48.cpp
@@ -1,1 +1,19 @@
+#include <string>
+#include <iomanip>
+#include <iostream>
+using namespace std;
 
+int main() {
+    int con = 1;
+    float num, so = 0;
+
+
+    while (con <= 7) {
+        cout << "Digite o " << con << " numero: ";
+        cin >> num;
+        so += num;
+        con++;
+    }
+    cout << "A soma dos 7 numeros e de: " << so << endl;
+    return 0;
+}
