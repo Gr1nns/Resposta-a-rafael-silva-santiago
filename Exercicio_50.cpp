43c91df1c2b99d20d561aaca50e4ed68678b3be9
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-02 15:14:56 -0300

    Update Exercicio_50.cpp

--- Exercicio_50.cpp
+++ Exercicio_50.cpp
@@ -1,1 +1,21 @@
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    srand(time(0));
+    int a = 1, nu, aci = 0, di = 0;
 
+    cout << "Numeros sorteados: ";
+
+    while (a++ <= 20) {
+        nu = rand() % 11;
+        cout << nu << " ";
+        if (nu > 5) aci++;
+        if (nu % 3 == 0) di++;
+    }
+    cout << "\nAcima de 5: " << aci << "\nDivisiveis por 3: " << di << endl;
+    return 0;
+}
