d1bb63ce1e2f711cfcc8da33af7523f3bdef14cd
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-06 19:02:23 -0300

    Update Exercicio_51.cpp

--- Exercicio_51.cpp
+++ Exercicio_51.cpp
@@ -1,1 +1,33 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
 
+	int vzs = 1;
+	float pre, me, ma;
+
+	while (vzs <= 8) {
+		cout << "Digite o preco do produto numero " << vzs << ": ";
+		cin >> pre;
+
+		if (vzs == 1) {
+
+			ma = pre;
+			me = pre;
+		}else {
+			if (pre > ma) {
+				ma = pre;
+			}
+			if (pre < me) {
+				me = pre;
+			}
+		}
+	     vzs++;
+	}
+	cout << fixed << setprecision(2);
+	cout << "Maior preco digitado: R$ " << ma << endl;
+	cout << "Menor preco digitado: R$ " << me << endl;
+
+	return 0;
+}
