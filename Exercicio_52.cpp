2509ecae8562cbc477f2cab8e7598b9da8cf6e32
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-08 11:25:55 -0300

    Update Exercicio_52.cpp

--- Exercicio_52.cpp
+++ Exercicio_52.cpp
@@ -1,1 +1,17 @@
-
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+   int id, sid, div, qu = 1;
+   
+   while(qu <= 10){
+   	cout<<"Digite sua idade: ";
+   	cin>>id;
+   	 sid += id;
+   	 qu++;
+   }
+   div = sid / 10;
+   cout<<"A media entre a idade das pessoas é de: "<<div<<endl;
+}
+// programa nao terminado q eu tenho q terminar
