5dac1abad43c0b28f3900cb4b81b2774b8c1b2d2
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-09 15:21:27 -0300

    Update Exercicio_71.cpp

--- Exercicio_71.cpp
+++ Exercicio_71.cpp
@@ -1,1 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+int main(){
+    int a, i;
+ cout<<"Digite o valor do array(8): ";
+cin>>a;
+int arr[a];
+for(i = 0; i < 8;i++){
+    arr[i]=999;
+}
+
+    return 0;
+}
