9e38816a73bfc0ee45d5ee1efd3e62afcbcf5247
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-15 19:34:07 -0300

    Update Exercicio_73.cpp

--- Exercicio_73.cpp
+++ Exercicio_73.cpp
@@ -1,1 +1,19 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    int a, i;
+    cout << "Digite o valor do array(10): ";
+    cin >> a;
+
+    if (a < 0) a = 0;
+    if (a >= 10) a = 10;
+
+    int arr[10];
+    for(i = 9; i >= 0; i--){
+        arr[i] = 1;
+    }
+    return 0;
+}
