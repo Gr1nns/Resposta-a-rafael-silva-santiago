e5b0feb219ebcdfe74c4018ac133bfc89747e277
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-15 11:50:04 -0300

    Update Exercicio_71.cpp

--- Exercicio_71.cpp
+++ Exercicio_71.cpp
@@ -3,15 +3,16 @@
 #include <string>
 using namespace std;
 
-
 int main(){
     int a, i;
- cout<<"Digite o valor do array(8): ";
-cin>>a;
-int arr[a];
-for(i = 0; i < 8;i++){
-    arr[i]=999;
-}
+    cout << "Digite o valor do array(8): ";
+    cin >> a;
+
+    int arr[a];
+
+    for(i = 0; i < 8; i++){
+        arr[i] = 999;
+    }
 
     return 0;
 }
