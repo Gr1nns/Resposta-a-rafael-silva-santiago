a28025178792873be68f5b6d2c0f4d843f165c7a
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-05-15 19:24:30 -0300

    Update Exercicio_72.cpp

--- Exercicio_72.cpp
+++ Exercicio_72.cpp
@@ -1,1 +1,21 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    int a, i,vs;
+    cout << "Digite o valor do array(10): ";
+    cin >> a;
+
+    if (a > 10) a = 10;
+    if (a <= 0) a = 1;
+    
+    int arr[a];
+    for(i = 0; i < a; i++){
+        arr[i] = 1;
+        vs=(i+1)*5;
+        cout<<vs<<endl;
+    }
+    return 0;
+}
