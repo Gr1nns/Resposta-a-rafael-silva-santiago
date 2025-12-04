7ac724a896fb34afd4314418401c76a20ad210bc
Grinn <152820684+Gr1nns@users.noreply.github.com>
2025-12-04 15:16:05 -0300

    Implement sum calculator with exit condition

--- Exercicio_56.cpp
+++ Exercicio_56.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n=0, soma=0;
+    cout<<"Aperte 0000, para parar o programa!"<<endl;
+while(true){
+    n=0;
+    cout<<"Digite numeros para serem somados: ";
+    cin>>n;
+    soma+=n;
+    if(n==0000){
+        break;
+    }
+}
+cout<<"A soma final de todos os numeros e de: "<<soma<<endl;
+    return 0;
+}
