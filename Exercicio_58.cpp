6c5b0080784770024345c1c03e71a93e00413961
Grinn <152820684+Gr1nns@users.noreply.github.com>
2026-02-20 21:55:44 -0300

    Update Exercicio_58.cpp

--- Exercicio_58.cpp
+++ Exercicio_58.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float idade=0, quant=0, total=0;
+    double media;
+    while(true){
+        idade = 0;
+        cout<<"Qual sua idade? ";
+        cin>>idade;
+        if(idade == 999){
+            break;
+        }
+        total += idade;
+        quant++;
+    }
+    media = total/quant;
+    cout<<"O total de alunos na sala é de: "<<quant<<" alunos"<<endl;
+    cout<<"A media da idade de todos os alunos é de: "<<fixed<<setprecision(2)<<media<<endl;
+    return 0;
+}
