691c4a6399c3cd9cc2f960520b24a0edbf202e88
Grinn <152820684+Gr1nns@users.noreply.github.com>
2026-02-22 15:46:09 -0300

    Update Exercicio_64.cpp

--- Exercicio_64.cpp
+++ Exercicio_64.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n1 =0;
+    while(n1 != 40){
+        cout<<n1<<endl;
+        n1+=5;
+        if(n1 == 40){
+            cout<<"40"<<endl;
+        }
+    }
+    cout<<"Acabou!"<<endl;
+    return 0;
+}
