69c267da22a56a19bd09c3f6fc22fa9df19f0f64
Grinn <152820684+Gr1nns@users.noreply.github.com>
2026-02-22 13:12:21 -0300

    Implement age input and statistics calculation

--- Exercicio_62.cpp
+++ Exercicio_62.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+int main(){
+    string continua="sim";
+    int idade, totalidade=0, soma=0, media=0, maiorQ21=0;
+    while(continua == "sim"){
+        cout<<"Digite sua idade: ";
+        cin>>idade;
+        totalidade++;
+        soma += idade;
+        if(idade>21){
+            maiorQ21++;
+        }
 
+        cout<<"Voce deseja continuar? (sim) (nao): ";
+        cin>>continua;
+    }
+    media = (double)soma / totalidade;
+    cout<<"Foram digitadas: "<<totalidade<<" idades"<<endl;
+    cout<<"A media das idade e de: "<<fixed<<setprecision(1)<<media;
+    cout<<"Tem "<<maiorQ21<<" pessoas com mais de 21 anos"<<endl;
+    return 0;
+}
