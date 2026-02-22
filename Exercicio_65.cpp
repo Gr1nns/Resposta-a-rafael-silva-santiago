e3d75f3fd1436bf953104d94e9e933885796c2a5
Grinn <152820684+Gr1nns@users.noreply.github.com>
2026-02-22 15:59:21 -0300

    Update Exercicio_65.cpp

--- Exercicio_65.cpp
+++ Exercicio_65.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n1 = 100;
+    cout<<"100"<<endl;
+    while(n1 != 0){
+        n1-=10;
+        cout<<n1<<endl;
+    }
+    return 0;
+}
